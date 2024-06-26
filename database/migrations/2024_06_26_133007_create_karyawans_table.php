1aabda46914b556d42e450a55e0ade6f5be5ba69
muhfachrurrozi <muhfachrurrozi@gmail.com>
2024-06-27 00:03:29 +0700

    first commit

---
+++ database/migrations/2024_06_26_133007_create_karyawans_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('divicions', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->boolean('active');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('divisi_id')->constrained('divicions');
+            $table->string('name');
+            $table->enum('status_karyawan', ['kontrak', 'training', 'tetep']);
+            $table->date('tanggal_masuk');
+            $table->date('tanggal_karyawan_tetap');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawans');
+        Schema::dropIfExists('divicions');
+    }
+};
